6c597a880efeb9891ede764851ffce957af842c3
fouad lutfi <thefouad@gmail.com>
2018-06-10 23:11:29 -0500

    done

---
+++ deepdive.js
@@ -0,0 +1,61 @@
+fouad = 'Covalence';
+var fouad; 
+
+console.log (fouad);
+
+setName();
+ function setName()   {
+var name = 'covalence';
+    console.log(fouad)}
+
+
+var name;
+
+let fruits = ['watermelon', 'orange', 'mango'];
+let favFruit;
+function printFruits() {
+    favFruit = fruits[0];
+    console.log(fruits[0]);
+}
+
+function printFavFruit() {
+   console.log(favFruit);
+}
+
+printFruits();
+printFavFruit();
+
+
+
+
+let fruits = ['watermelon', 'orange', 'mango'];
+
+function printFruits() {
+    let favFruit = fruits[2];
+    console.log(fruits[0]);
+}
+
+function printFavFruit() {
+   console.log(favFruit);
+}
+
+printFruits();
+printFavFruit();
+
+let fruits = ['banana', 'orange', 'pineapple'];
+
+function printFruits() {
+   let favFruit = fruits[2];
+  console.log(fruits[0]);
+
+ function printFavFruit() {
+      console.log(favFruit);
+    }
+}
+
+printFruits();
+
+var color = purple;
+function color(){
+    if (color=blue);
+}
